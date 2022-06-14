60b3ab71eb372ad0f8770702430cd9271c4b9e89
AlgoritmikaPodolsk <76789093+AlgoritmikaPodolsk@users.noreply.github.com>
2022-06-14 23:07:15 +0300

    tests

--- test_kata.py
+++ test_kata.py
@@ -1,14 +1,20 @@
 import kata
 import unittest
 
-test.describe("Basic tests")
-test.assert_equals(generate_hashtag(''), False, 'Expected an empty string to return False')
-test.assert_equals(generate_hashtag('Do We have A Hashtag')[0], '#', 'Expeted a Hashtag (#) at the beginning.')
-test.assert_equals(generate_hashtag('Codewars'), '#Codewars', 'Should handle a single word.')
-test.assert_equals(generate_hashtag('Codewars      '), '#Codewars', 'Should handle trailing whitespace.')
-test.assert_equals(generate_hashtag('Codewars Is Nice'), '#CodewarsIsNice', 'Should remove spaces.')
-test.assert_equals(generate_hashtag('codewars is nice'), '#CodewarsIsNice', 'Should capitalize first letters of words.')
-test.assert_equals(generate_hashtag('CodeWars is nice'), '#CodewarsIsNice', 'Should capitalize all letters of words - all lower case but the first.')
-test.assert_equals(generate_hashtag('c i n'), '#CIN', 'Should capitalize first letters of words even when single letters.')
-test.assert_equals(generate_hashtag('codewars  is  nice'), '#CodewarsIsNice', 'Should deal with unnecessary middle spaces.')
-test.assert_equals(generate_hashtag('Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat'), False, 'Should return False if the final word is longer than 140 chars.')
+
+class Test_Generate_Hashtag(unittest.TestCase):
+    def tests(self):
+        # self.assertEqual(inc_dec.increment(3), 4)
+        self.assertEquals(kata.generate_hashtag(''), False, 'Expected an empty string to return False')
+        self.assertEquals(kata.generate_hashtag('Do We have A Hashtag')[0], '#', 'Expeted a Hashtag (#) at the beginning.')
+        self.assertEquals(kata.generate_hashtag('Codewars'), '#Codewars', 'Should handle a single word.')
+        self.assertEquals(kata.generate_hashtag('Codewars      '), '#Codewars', 'Should handle trailing whitespace.')
+        self.assertEquals(kata.generate_hashtag('Codewars Is Nice'), '#CodewarsIsNice', 'Should remove spaces.')
+        self.assertEquals(kata.generate_hashtag('codewars is nice'), '#CodewarsIsNice', 'Should capitalize first letters of words.')
+        self.assertEquals(kata.generate_hashtag('CodeWars is nice'), '#CodewarsIsNice', 'Should capitalize all letters of words - all lower case but the first.')
+        self.assertEquals(kata.generate_hashtag('c i n'), '#CIN', 'Should capitalize first letters of words even when single letters.')
+        self.assertEquals(kata.generate_hashtag('codewars  is  nice'), '#CodewarsIsNice', 'Should deal with unnecessary middle spaces.')
+        self.assertEquals(kata.generate_hashtag('Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat'), False, 'Should return False if the final word is longer than 140 chars.')
+
+if __name__ == '__main__':
+    unittest.main()
